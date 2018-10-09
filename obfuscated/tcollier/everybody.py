dfa22ee10db7a1b1cf7258793192ac248410c923
Tom Collier <collier@apartmentlist.com>
2018-10-08 17:00:41 -0700

    Add ruby entry

---
+++ obfuscated/tcollier/everybody.py
@@ -0,0 +1,19 @@
+#!/./usr/.././usr/./bin/../../usr/./bin/./env ruby  -a --enable=did_you_mean
+#!/bin/rb2py/python --source-language ruby --compat-lib englishnames chinese
+#!~turbopack~>while(true);a=a[-1::-1];gzip;git+https://myname.co.cn/汤姆科利尔
+
+hash=%w[usdp shir raco frtp hefe
+afar  grin  kara tomm  name apts
+foaf];a||=?1;a+=(a .to_i+1).to_s
+           a+=(a.to_i+          19).to_s        ;b=(a[0..1].to_i/a[2].to_i)
+           .times.map(      &:to_s);five={}.    length; hash.count{ |mi|;mi.
+           chars.send(    *%i[send    detect])  {|me|b[  five%4]+=  me;(#//#
+           five+=1) &&   nil}};;c=    b.map{|c| ;c.sub(  %r{(\d)},  ?/).sub(
+           /f.*/,'')}.   join;arm=    'head';;; leg=arm  ;(head=?t  +'ail');
+           d=__FILE__.    split(?/    )[0-1-0]  .chars-  'do.it!!'  .chars;;
+           d.shift&&d.      pop;d.insert(2,1    &&2&&d.  shift);e=  d[-3];g=
+           d[4 ]=d[5];          d[-2]=e;        g=d.map  {|f|f.ord  - 0b1101
+
+};g[-1]-=2** 5;h= {'X'=>"11"+"4",'U'=>"1"*3};h=g.map{|k|;(h[k.chr]||k).to_i.
+chr};phone=->(*i){i.reduce(''){|k,l|;k+=l+(2*4 **2).chr};};q=phone.call(leg,
+?-+a,c,?|,head,?-+a[0]);puts phone.call(`#{q}`[0..2],h.reverse.join.itself);
