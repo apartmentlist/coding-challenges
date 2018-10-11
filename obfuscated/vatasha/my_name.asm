8488da7d2791527860cb3c72e2a4e1c32e4833f7
Vatasha White <vwhite@apartmentlist.com>
2018-10-10 23:00:51 -0700

    finally fixed spacing

--- obfuscated/vatasha/my_name.asm
+++ obfuscated/vatasha/my_name.asm
@@ -7,21 +7,18 @@ SECTION .text
 global start    
 	
 start:
-   push ebp             ;establish stack frame, push base pointer (ebp) onto stack to save it
-   mov ebp, esp	        ;copy stack pointer esp to ebp so ebp points to stack frame 
-   push	dword msg_len   ;push msg length to stack
-   push dword msg    	;push pointer to msg onto stack
-   push dword 1       	;file descriptor (stdout)
-   mov eax, 4  		;system call number (sys_write)
-   sub esp, 4           ;space allocated on stack for msg var  
-   int 0x80          	;call kernel
-   add esp, 16          ;pop stack 4 times (look to prev pushes above)
+	push ebp             	;establish stack frame, push base pointer (ebp) onto stack to save it
+	mov ebp, esp	        ;copy stack pointer esp to ebp so ebp points to stack frame 
+	push dword msg_len   	;push msg length to stack
+	push dword msg    	;push pointer to msg onto stack
+	push dword 1       	;file descriptor (stdout)
+	mov eax, 4  		;system call number (sys_write)
+	sub esp, 4           	;space allocated on stack for msg var  
+	int 0x80          	;call kernel
+	add esp, 16          	;pop stack 4 times (look to prev pushes above)
   		
-   
-   push dword 0         ;exit code
-   mov eax, 1		;mac0S termination code 
-   sub esp, 4           ;space allocated exit code onto stack
-   int 0x80
+	push dword 0         	;exit code
+	mov eax, 1		;mac0S termination code 
+	sub esp, 4           	;space allocated exit code onto stack
+	int 0x80		;call kernel
 
-
- 
